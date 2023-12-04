d9f6398347f49fa02078a2eda45658c24127f92a
paraponera <35348494+zoleng@users.noreply.github.com>
2023-12-04 18:51:30 +0100

    Update day04.py

--- day04.py
+++ day04.py
@@ -1,4 +1,10 @@
-file = open("day04.txt", "r")
+file = ""
+linesArray = ""
+
+def readAndMakearray():
+    global file, linesArray
+    file = open("day04-testFile.txt")
+    linesArray = file.readlines()
 
 #part 1
 
@@ -24,6 +30,7 @@ def counterMatchAndCalculRes(line):
         else:
             num_count_final = num_count_final * 2
     final_count = final_count + num_count_final
+    print("finale count " + str(final_count))
     num_count_final = 0
 
 def part1():
@@ -47,23 +54,48 @@ def part1():
                         """)
     print("Final result : " + str(final_count))
 
-part1()
+#part1()
 
 #part 2
 
-#a chaque res call la fonction de part 1 x fois pour nb reponse
 
+#ajouter a cahnque ligne une carte nb carte, et a chaque tour ajouter a ce chiffre le nb de fois qu'on repete
 def part2():
-    global final_count, count, num_count_final, num_point_multi
-    for line in file:
-        one_go = [name.strip() for name in line.split(':')[1].split('|')]
+    global final_count, count, num_count_final, num_point_multi, linesArray, file
+    num_card_each_card = 0
+    for num_line in range(0, len(linesArray), 1):
+   # for num_line, line in len(linesArray):
+        one_go = [name.strip() for name in linesArray[num_line].split(':')[1].split('|')]
         one_go_new = []
         for i in one_go:
             j = i.replace('  ',' ')
             one_go_new.append(j)
+        one_go_new.append(0)
         list_one = list(one_go_new[0].split(" "))
         list_two = list(one_go_new[1].split(" "))
         matches = list(set(list_one).intersection(list_two))
+        print(len(matches))
         for i in range(0, len(matches), 1):
-            counterMatchAndCalculRes(line)# send la ligne d'apres du coup changer le for
+            
+            num_line_to_read = int(num_line) + int(i) + int(1)
+            new_line = linesArray[num_line_to_read]
+            print(new_line + " new_line")
+            counterMatchAndCalculRes(new_line)# send la ligne d'apres du coup changer le for
+    print("""\
+    ,-.       _,---._     
+    /  )    .-'       `. _____
+    (  (   ,'            `   /|
+    \  `-"            \ \   / |
+    `.              ,  \ \ /  |
+    /`.          ,'-`----Y    |
+    (            ;        |   '
+    |  ,-.    ,-'         |  /
+    |  | (   |            | /
+    )  |  \  `.___________|/
+    `--'   `--'
 
+                        """)
+    print("Final result : " + str(final_count))
+
+readAndMakearray()
+part2()
