1e59c75af5c659342ac6556c43d2d909a6dd4de0
paraponera <35348494+zoleng@users.noreply.github.com>
2023-12-10 00:30:57 +0100

    Update day03.py maybe one day it will be a switch case

--- day03.py
+++ day03.py
@@ -1,4 +1,3 @@
-
 import sys 
 import re
 import numpy as np
@@ -24,100 +23,54 @@ def solverPart1():
     for i in range(0, len(arr), 1):
         for j in range(0, len(arr[i]), 1):
             if arr[i][j] in allSpeChar:
-                num = ""
-                num1 = ""
-                num2 = ""
-                together = False
-                if arr[i - 1][j].isdigit():
-                  num = arr[i - 1][j]
-                  together = True
-                pos = True
-                neg = True
-                for x in range(1, len(arr[j]), 1): 
-                 #   print(arr[i + 1][j + x])
-                    if arr[i - 1][j + x].isdigit() and pos == True:
-                        num2 = num2 + arr[i - 1][j + x]
-                    if arr[i - 1][j + x].isdigit() == False:
-                        pos = False
-                    if arr[i - 1][j - x].isdigit() == False:
-                        neg = False
-                    if arr[i - 1][j - x].isdigit() and neg == True:
-                        num1 = arr[i - 1][j - x] + num1
-                        neg = True
-                    if pos == False and neg == False:
-                        break
-                if together:
-                    num = num1 + num + num2
-                    print(num)
-                    if num != "":
-                        final_num += int(num)
-                else:
-                    if num1 != "":
-                        final_num += int(num1)
-                    if num2 != "":
-                        final_num += int(num2)
-                    print(num1)
-                    print(num2)
-                num = ""
-                num1 = ""
-                num2 = ""
-                together = False
-                if arr[i + 1][j].isdigit():
-                  num = arr[i + 1][j]
-                  together = True
-                pos = True
-                neg = True
-                for x in range(1, len(arr[j]), 1): 
-                 #   print(arr[i + 1][j + x])
-                    if arr[i + 1][j + x].isdigit() and pos == True:
-                        num2 = num2 + arr[i + 1][j + x]
-                    if arr[i + 1][j + x].isdigit() == False:
-                        pos = False
-                    if arr[i + 1][j - x].isdigit() == False:
-                        neg = False
-                    if arr[i + 1][j - x].isdigit() and neg == True:
-                        num1 = arr[i + 1][j - x] + num1
-                        neg = True
-                    if pos == False and neg == False:
-                        break
-                if together:
-                    num = num1 + num + num2
-                    print(num)
-                    if num != "":
-                        final_num += int(num)
-                else:
-                    if num1 != "":
-                        final_num += int(num1)
-                    if num2 != "":
-                        final_num += int(num2)
-                    print(num1)
-                    print(num2)
-                num = ""
-                num1 = ""
-                num2 = ""
-                together = False
-                pos = True
-                neg = True
-                for x in range(1, len(arr[j]), 1): 
-                 #   print(arr[i + 1][j + x])
-                    if arr[i][j + x].isdigit() and pos == True:
-                        num2 = num2 + arr[i][j + x]
-                    if arr[i][j + x].isdigit() == False:
-                        pos = False
-                    if arr[i][j - x].isdigit() == False:
-                        neg = False
-                    if arr[i][j - x].isdigit() and neg == True:
-                        num1 = arr[i][j - x] + num1
-                        neg = True
-                    if pos == False and neg == False:
-                        break
-                if num1 != "":
-                    final_num += int(num1)
-                if num2 != "":
-                    final_num += int(num2)
-                print(num1)
-                print(num2)
-    print(final_num)
+                for upMidDown in range(-1, 2, 1): 
+                    num = ""
+                    num1 = ""
+                    num2 = ""
+                    together = False
+                    if arr[i + upMidDown][j].isdigit():
+                        num = arr[i + upMidDown][j]
+                        together = True
+                    pos = True
+                    neg = True
+                    for x in range(1, len(arr[j]), 1): 
+                       # print(arr[i + upMidDown][j + x])
+                        if arr[i + upMidDown][j + x].isdigit() and pos == True:
+                            num2 = num2 + arr[i + upMidDown][j + x]
+                        if  arr[i + upMidDown][j + x].isdigit() == False:
+                            pos = False
+                        if  arr[i + upMidDown][j - x].isdigit() == False:
+                            neg = False
+                        if  arr[i + upMidDown][j - x].isdigit() and neg == True:
+                            num1 = arr[i + upMidDown][j - x] + num1
+                            neg = True
+                        if pos == False and neg == False:
+                            break
+                    if together:
+                        num = num1 + num + num2
+                        if num != "":
+                            final_num += int(num)
+                    else:
+                        if num1 != "":
+                            final_num += int(num1)
+                        if num2 != "":
+                            final_num += int(num2)
+       
+    print("""\
+    ,-.       _,---._     
+    /  )    .-'       `. _____
+    (  (   ,'            `   /|
+    \  `-"            \ \   / |
+    `.              ,  \ \ /  |
+    /`.          ,'-`---- Y   |
+    (            ;        |   '
+    |  ,-.    ,-'         |  /
+    |  | (   |            | /
+    )  |  \  `.___________|/
+    `--'   `--'
+
+                        """)
+    print("Final result : " + str(final_num))
                 
 
 
